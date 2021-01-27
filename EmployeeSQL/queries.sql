813a5bc0564b66e7f5c628f701ba841aae618555
abhijitpurru2 <68308831+abhijitpurru2@users.noreply.github.com>
2021-01-27 15:21:17 -0500

    Add files via upload

--- EmployeeSQL/queries.sql
+++ EmployeeSQL/queries.sql
@@ -1,9 +1,14 @@
 -- 1. List the following details of each employee: employee number, last name, first name, sex, and salary.
 SELECT emp.emp_no, emp.last_name, emp.first_name, emp.sex, sal.salary
 FROM employees emp, salaries sal
-WHERE emp.emp_no = sal.emp_no
+WHERE emp.emp_no = sal.emp_no;
 
 -- 2. List first name, last name, and hire date for employees who were hired in 1986.
 SELECT first_name, last_name, hire_date
 FROM employees
-WHERE hire_date >= '1986-01-01' and hire_date <= '1986-12-31'
+WHERE hire_date >= '1986-01-01' and hire_date <= '1986-12-31';
+
+-- 3. List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name.
+SELECT man.dept_no, dep.dept_name, man.emp_no, emp.first_name, emp.last_name
+FROM dept_manager man, departments dep, employees emp
+WHERE man.dept_no = dep.dept_no AND man.emp_no = emp.emp_no;
