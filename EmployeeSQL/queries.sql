3abd1e19f2e63d3c94afccec6fdd9d22616b1bfa
abhijitpurru2 <68308831+abhijitpurru2@users.noreply.github.com>
2021-01-27 14:58:21 -0500

    Add files via upload

--- EmployeeSQL/queries.sql
+++ EmployeeSQL/queries.sql
@@ -1,3 +1,9 @@
+-- 1. List the following details of each employee: employee number, last name, first name, sex, and salary.
 SELECT emp.emp_no, emp.last_name, emp.first_name, emp.sex, sal.salary
 FROM employees emp, salaries sal
 WHERE emp.emp_no = sal.emp_no
+
+-- 2. List first name, last name, and hire date for employees who were hired in 1986.
+SELECT first_name, last_name, hire_date
+FROM employees
+WHERE hire_date >= '1986-01-01' and hire_date <= '1986-12-31'
